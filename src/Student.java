17fbca6a08270fb8850ec90affa42c00da0586b9
briscoep12 <briscoep12@gmail.com>
2026-09-01 10:38:40 -0400

    Added parameter constructors.

--- src/Student.java
+++ src/Student.java
@@ -11,16 +11,38 @@ public class Student {
     //JVM access the static method using ClassName.MethodNa,e
     //JVM executes Student.main
 
-    public static void main(String[] args){
 
     //If constructors aren't defined, JVM will provide a default constructor.
+    //Constructors don't have a return type
+    //Constructors must have the same name as the class name.
+    Student(){
+        FN="John";
+        LN= "Jonas";
+        SID = 111;
+
+    }
+
+    Student(String FN, String LN, int SID){
+        this.FN = FN;
+        this.LN= LN;
+        this.SID= SID;
+    }
+    public static void main(String[] args){
+
+
     //Creates reference value for class
         Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student("Bukumi","Jake",111);
+
+        IO.println("First name is: " + s3.FN);
+        IO.println("Last name is: " + s3.LN);
+       IO.println("The Student ID is: " + s3.SID);
 
     //To select attributes use "."
-        IO.println("First name is: " + s1.FN);
-        IO.println("Last name is: " + s1.LN);
-        IO.println("The Student ID is: " + s1.SID);
+//        IO.println("First name is: " + s1.FN);
+//        IO.println("Last name is: " + s1.LN);
+//        IO.println("The Student ID is: " + s1.SID);
 
 
 
