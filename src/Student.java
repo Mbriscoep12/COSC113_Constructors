a5336b9e43fe736f392a634efc165d7e5ddd2d26
briscoep12 <briscoep12@gmail.com>
2026-09-01 09:32:44 -0400

    Added ability to access attributes.

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,31 @@
 public class Student {
+    //Attributes
+    String FN;
+    String LN;
+    int SID;
+
+    //Methods can be static and non-static.
+    //This method returns nothing - void
+
+    //Method name - main
+    //JVM access the static method using ClassName.MethodNa,e
+    //JVM executes Student.main
 
     public static void main(String[] args){
 
-        System.out.printf("Hello and welcome!");
+    //If constructors aren't defined, JVM will provide a default constructor.
+    //Creates reference value for class
+        Student s1 = new Student();
+
+    //To select attributes use "."
+        IO.println("First name is: " + s1.FN);
+        IO.println("Last name is: " + s1.LN);
+        IO.println("The Student ID is: " + s1.SID);
+
+
+
+
+        System.out.println("Hello Constructors");
     }
 
 }
